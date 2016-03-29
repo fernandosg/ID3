cd49943f25bee56ea40d870e3291dc3796f806f5
fernando <fsg_tmp@hotmail.com>
2016-03-29 14:28:19 -0600

    Modification for calculate the entropy of set and calculate of the set entropy considering the attributes,with recursive way to iterate over these calculations

--- src/id3.js
+++ src/id3.js
@@ -36,17 +36,16 @@ var ID3=(function(){
 		return entropy_class;
 	}
 	function calculateEntropySet(set){
-		var entropy=0,homogeneous=false;;
+		var entropy=1,homogeneous=false;
+
 		for(var i=0,name_of_class=attributes[class_defined][i],length=attributes[class_defined].length;i<length;i++,name_of_class=attributes[class_defined][i]){
-			if(set[name_of_class]==set["total"]){
-				homogeneous=true;
-				break;
-			}
+			if(set[name_of_class]==set["total"])				
+				break;			
 			entropy=entropy-(set[name_of_class]/set["total"])*Math.log2((set[name_of_class]/set["total"]));
 		}
-		return {entropy:entropy,ishomogeneous:homogeneous}
+		return {entropy:(!isNaN(entropy) ? entropy : 1)};
 	}
-	function calculateFrequencies(){
+	function calculateFrequencies(table_training){
 		var table_frequencies={},row_frequencies={};		
 		for(var x=0,attributes_label_length=attributes_label.length;x<attributes_label_length;x++){
 			table_frequencies[attributes_label[x]]={};
@@ -59,40 +58,99 @@ var ID3=(function(){
 					if(table_training[i][attributes_label[x]]==attributes[attributes_label[x]][y])
 						row_frequencies[attributes_label[x]][attributes[attributes_label[x]][y]].push(table_training[i]);
 				table_frequencies[attributes_label[x]][attributes[attributes_label[x]][y]]=calculateFrequencieClass(row_frequencies[attributes_label[x]][attributes[attributes_label[x]][y]]);				
+				table_frequencies[attributes_label[x]][attributes[attributes_label[x]][y]]["table"]=row_frequencies[attributes_label[x]][attributes[attributes_label[x]][y]];
 				row_frequencies={};
 			}
 		}
 		return table_frequencies;
 	}
-	function execute(){
-		table_frequencies=calculateFrequencies();		
-		frequencie_class_for_set=calculateFrequencieClass(table_training);		
-		entropy_of_set=calculateEntropySet(frequencie_class_for_set);		
-		/* Calculate the entropies of each of the attributes in the set */		
-		entropies=[];
-		for(var i=0,attribute_label_name=attributes_label[i],attributes_label_length=attributes_label.length;i<attributes_label_length;i++,attribute_label_name=attributes_label[i]){
-			if(attributes_label[i]==class_defined)
-				continue;
-			table_frequencies[attribute_label_name]["entropy"]=0
-			for(var x=0,attributes_length=attributes[attribute_label_name].length;x<attributes_length;x++){
-				table_frequencie=table_frequencies[attribute_label_name][attributes[attribute_label_name][x]];
-				table_frequencies[attribute_label_name]["entropy"]+=(table_frequencie["total"]/frequencie_class_for_set["total"])*calculateEntropySet(table_frequencie)["entropy"];
+	function reduceSet(attribute_name,repeat,table_training){
+		new_table=[];
+		if(repeat<7 && attributes[attribute_name]!=undefined){
+			for(var i=0,length=attributes[attribute_name].length;i<length;i++){
+				for(x=0,length_table=table_training.length;x<length_table;x++){
+					if(table_training[x][attribute_name]==attributes[attribute_name][i]){
+						 //EN LUGAR DE ELIMINAR AQUI EL ATRIBUTO CON MAYOR GANANCIA
+						 // MEJOR ELIMINAR EL ATRIBUTO DEL attributes_label
+						attributes_label=attributes_label.filter(function(element){
+							return element!=attribute_name;
+						});
+						new_table.push(table_training[x]);
+					}
+				}				
+				repeat++;
+				if(!isHomogeneous(new_table,attributes[attribute_name][i]))
+					execute(repeat,new_table);
+				else{
+					attributes[attribute_name].splice(i,1);	
+					i=i-1;	
+					length=attributes[attribute_name].length;
+				}
+				new_table=[];
 			}
-			table_frequencies[attribute_label_name]["entropy"]=entropy_of_set["entropy"]-table_frequencies[attribute_label_name]["entropy"];
-			entropies.push(table_frequencies[attribute_label_name]["entropy"]);
 		}
+	}
+	/* Filter all the elements with the same class name, if there are more that one where the
+		the class name are differente,is because this is no homogeneous
+	*/
+	function isHomogeneous(table_frequencies,higher_attribute){
+		var key="",attribute_homogeneous_value="";
+		filter=table_frequencies.filter(function(element){
+			attribute_homogeneous_value=element[higher_attribute];
+			if(key.length>0){
+				return key!=element[class_defined];
+			}else{
+				key=element[class_defined];
+				return false;
+			}
+		});
+		return filter.length==0;
+	}
+	/* If the set are only two elements, there is not reason to calculate the entropy */
+	function splitSet(table_training){
+		console.log("The last are ")
+		console.dir(table_training[0]);
+		console.dir(table_training[1]);
+	}
+	function execute(repeat,table_training){
+		table_frequencies=calculateFrequencies(table_training);		
+		frequencie_class_for_set=calculateFrequencieClass(table_training);		
+		entropy_of_set=calculateEntropySet(frequencie_class_for_set);	
+		/* Calculate the entropies of each of the attributes in the set */	
+		higher_entropy=0;	
+		higher_entropy_name="";
+		entropies=[];
+		if(table_training.length>2){
+			for(var i=0,attribute_label_name=attributes_label[i],attributes_label_length=attributes_label.length;i<attributes_label_length;i++,attribute_label_name=attributes_label[i]){
+				if(attributes_label[i]==class_defined)
+					continue;			
+				table_frequencies[attribute_label_name]["entropy"]=0
+				for(var x=0,attributes_length=attributes[attribute_label_name].length;x<attributes_length;x++){
+					table_frequencie=table_frequencies[attribute_label_name][attributes[attribute_label_name][x]];
+					table_frequencies[attribute_label_name]["entropy"]+=(table_frequencie["total"]/frequencie_class_for_set["total"])*calculateEntropySet(table_frequencie)["entropy"];
+				}
+				table_frequencies[attribute_label_name]["entropy"]=entropy_of_set["entropy"]-table_frequencies[attribute_label_name]["entropy"];			
+				if(higher_entropy<table_frequencies[attribute_label_name]["entropy"]){
+					higher_entropy=table_frequencies[attribute_label_name]["entropy"];
+					higher_entropy_name=attribute_label_name;
+				}			
+			}
+			reduceSet(higher_entropy_name,repeat,table_training);
+		}else
+			splitSet(table_training);		
+		
 	}
 	return{
 		init:function(data){
 			Math.log2 = Math.log2 || function(x){return Math.log(x)*Math.LOG2E;};
 			class_defined=data;
-			execute();
+			execute(1,table_training);
 		},		
 		getFrequencieClass:function(){
 			console.dir(calculateFrequencieClass(table_training));
 		},
 		getFrequencies:function(){
-			console.dir(calculateFrequencies());
+			console.dir(calculateFrequencies(table_training));
 		},
 		getEntropy:function(){
 			console.dir(calculateEntropySet({ total: 14, no: 4, si: 10 }));
