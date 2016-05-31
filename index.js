769fe527970bd1042d71e91bf709b1f9a5403579
fernando <fsg_tmp@hotmail.com>
2016-05-31 02:52:42 -0500

    A new version stable, the algorithm works fine,just need to use in frontend for create the tables of frequencies

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express=require("express");
 var app=express();
-id3=require("./src/id3.js");
+ID3=require("./src/id3.js");
 app.listen(3000);
+id3=new ID3();
+id3.init("Play ball");
+id3.initBuild();
+//id3.getTree(id3.getEntrenamiento());
 app.get("/",function(req,response){
 	id3.init("juego_tenis");
-	id3.getTree();
+	id3.getTree(id3.getEntrenamiento());
 	//id3.getEntropy();
 	//id3.getFrequencies();
 	response.send("This is awesome");
--- src/id3.js
+++ src/id3.js
@@ -1,15 +1,23 @@
-var ID3=(function(){
-	var class_defined;
-	Tree=new require("./class/tree.js");
-	var attributes_label=["estado","humedad","viento","juego_tenis"];
-	var attributes={
-		"estado":["soleado","nublado","lluvia"],
-		"humedad":["alta","normal"],
-		"viento":["leve","fuerte"],
-		"juego_tenis":["si","no"]
-	}
-	var table_training=[
-		{"estado":"soleado","humedad":"alta","viento":"leve","juego_tenis":"no"},
+function id3(){
+
+
+this.tabla_entrenamiento=[
+{"Outlook":"Sunny","Temperature":"Hot","Humidity":"High","Wind":"Weak","Play ball":"No"},
+{"Outlook":"Sunny","Temperature":"Hot","Humidity":"High","Wind":"Strong","Play ball":"No"},
+{"Outlook":"Overcast","Temperature":"Hot","Humidity":"High","Wind":"Weak","Play ball":"Yes"},
+{"Outlook":"Rain","Temperature":"Mild","Humidity":"High","Wind":"Weak","Play ball":"Yes"},
+{"Outlook":"Rain","Temperature":"Cool","Humidity":"Normal","Wind":"Weak","Play ball":"Yes"},
+{"Outlook":"Rain","Temperature":"Cool","Humidity":"Normal","Wind":"Strong","Play ball":"No"},
+{"Outlook":"Overcast","Temperature":"Cool","Humidity":"Normal","Wind":"Strong","Play ball":"Yes"},
+{"Outlook":"Sunny","Temperature":"Mild","Humidity":"High","Wind":"Weak","Play ball":"No"},
+{"Outlook":"Sunny","Temperature":"Cool","Humidity":"Normal","Wind":"Weak","Play ball":"Yes"},
+{"Outlook":"Rain","Temperature":"Mild","Humidity":"Normal","Wind":"Weak","Play ball":"Yes"},
+{"Outlook":"Sunny","Temperature":"Mild","Humidity":"Normal","Wind":"Strong","Play ball":"Yes"},
+{"Outlook":"Overcast","Temperature":"Mild","Humidity":"High","Wind":"Strong","Play ball":"Yes"},
+{"Outlook":"Overcast","Temperature":"Hot","Humidity":"Normal","Wind":"Weak","Play ball":"Yes"},
+{"Outlook":"Rain","Temperature":"Mild","Humidity":"High","Wind":"Strong","Play ball":"No"}];
+	/*this.tabla_entrenamiento=[
+		{"outlook":"sunny","temperature":"hot","humidity":"high","wind":"weak","juego_tenis":"no"},
 		{"estado":"soleado","humedad":"alta","viento":"fuerte","juego_tenis":"no"},
 		{"estado":"nublado","humedad":"alta","viento":"leve","juego_tenis":"si"},
 		{"estado":"lluvia","humedad":"alta","viento":"leve","juego_tenis":"si"},
@@ -23,156 +31,188 @@ var ID3=(function(){
 		{"estado":"nublado","humedad":"alta","viento":"fuerte","juego_tenis":"si"},
 		{"estado":"nublado","humedad":"normal","viento":"leve","juego_tenis":"si"},
 		{"estado":"lluvia","humedad":"alta","viento":"fuerte","juego_tenis":"si"}
-	];
-	function calculateFrequencieClass(table_training_pam){
-		var entropy_class={};
-		entropy_class["total"]=0;
-		for(var x=0,table_length=table_training_pam.length;x<table_length;x++){			
-			if(entropy_class[table_training_pam[x][class_defined]]==undefined)
-				entropy_class[table_training_pam[x][class_defined]]=1;
-			else
-				entropy_class[table_training_pam[x][class_defined]]+=1;
-			entropy_class["total"]+=1;
-		}	
-		return entropy_class;
-	}
-	function calculateEntropySet(set){
-		var entropy=1,homogeneous=false;
+	];*/
+}
 
-		for(var i=0,name_of_class=attributes[class_defined][i],length=attributes[class_defined].length;i<length;i++,name_of_class=attributes[class_defined][i]){
-			if(set[name_of_class]==set["total"])				
-				break;			
-			entropy=entropy-(set[name_of_class]/set["total"])*Math.log2((set[name_of_class]/set["total"]));
+
+id3.prototype.init=function(clase){
+	this.clase=clase;
+	this.atributos_nombres=["Outlook","Temperature","Humidity","Wind","Play ball"];
+	this.atributos={
+		"Outlook":["Sunny","Overcast","Rain"],
+		"Temperature":["Hot","Mild","Cool"],
+		"Humidity":["High","Normal"],
+		"Wind":["Weak","Strong"],
+		"Play ball":["Yes","No"]
+	}
+	
+}
+
+id3.prototype.getEntrenamiento=function(){
+	return this.tabla_entrenamiento;
+}
+
+
+id3.prototype.getFrecuencias=function(atributo,atributos,tabla_entrenamiento){
+	var parent=this;
+	tabla_frecuencia={};
+	tabla_frecuencia[atributo]={}; 
+		//Iteracion sobre las clases SI y NO
+		atributos[atributo].forEach(function(atributo_itera){
+			//Iteracion sobre cada atributo (iteracion sobre soleado,nublado,lluvia de "estado")
+			tabla_frecuencia[atributo][atributo_itera]={};
+			atributos[parent.clase].forEach(function(clase_dominio){
+				tabla_frecuencia[atributo][atributo_itera][clase_dominio]=0;	
+			})
+		});			
+	tabla_entrenamiento.forEach(function(entrada){
+		tabla_frecuencia[atributo][entrada[atributo]][entrada[parent.clase]]++;			
+	})	
+	return tabla_frecuencia;
+}
+
+id3.prototype.conteoTotales=function(atributo,tabla_frecuencia){
+	this.atributos[atributo].forEach(function(atributo_itera){
+		tabla_frecuencia[atributo_itera]["total"]=0;
+		parent.atributos[parent.clase].forEach(function(clase_dominio){		
+			tabla_frecuencia[atributo_itera]["total"]+=tabla_frecuencia[atributo_itera][clase_dominio];
+		})
+	})
+	
+	return tabla_frecuencia;
+}
+
+id3.prototype.conteoEntrenamiento=function(entrenamiento){
+	tabla_conteo={};
+	tabla_conteo["total"]=0;
+	parent=this;
+	this.atributos[this.clase].forEach(function(clase_dominio){
+		tabla_conteo[clase_dominio]=0;		
+	})
+	entrenamiento.forEach(function(entrada){
+		tabla_conteo[entrada[parent.clase]]++;
+		tabla_conteo["total"]++;
+	})
+	return tabla_conteo;
+}
+
+id3.prototype.getEntropia=function(conjunto){
+	//DEBO DE HACER conteoTotales para que pueda contar cualquier tipo de conjunto
+	//conjunto de entrenamiento y conjunto de atributo
+	entropia=0,valor=0;
+	this.atributos[this.clase].forEach(function(clase_dominio){
+		if(conjunto[clase_dominio]!=0 && conjunto["total"]!=0)
+			entropia-=(conjunto[clase_dominio]/conjunto["total"])*(Math.log(conjunto[clase_dominio]/conjunto["total"])/Math.log(2));
+		else
+			entropia=0;		
+	})
+	return entropia;
+}
+
+id3.prototype.getEntropiaAtributo=function(atributo,conjunto){
+	parent=this;
+	entropia=0;
+	this.atributos[atributo].forEach(function(atributo_itera){
+		entropia+=(conjunto[atributo_itera]["total"]/parent.tabla_entrenamiento.length)*parent.getEntropia(conjunto[atributo_itera]);
+	})
+	return entropia;
+}
+
+probar_homogenea=[{ estado: 'nublado',
+    humedad: 'alta',
+    viento: 'leve',
+    juego_tenis: 'si' },
+  { estado: 'nublado',
+    humedad: 'normal',
+    viento: 'fuerte',
+    juego_tenis: 'si' },
+  { estado: 'nublado',
+    humedad: 'alta',
+    viento: 'fuerte',
+    juego_tenis: 'si' },
+  { estado: 'nublado',
+    humedad: 'normal',
+    viento: 'leve',
+    juego_tenis: 'si' } ]
+
+
+id3.prototype.filtradoAtributo=function(atributo,tabla_entrenamiento,atributos_nombres){
+	parent=this;
+	tablas_filtradas=[];	
+	this.atributos[atributo].forEach(function(atributo_itera){
+		nueva_tabla=[];
+		//console.log("atributo_itera= "+atributo_itera+" "+atributo);
+		if(atributo_itera!=parent.clase){
+			tabla_entrenamiento.forEach(function(entrada){
+				if(entrada[atributo]==atributo_itera)
+					nueva_tabla.push(entrada);
+			})
+			console.log("------------------\n\n");
 		}
-		return {entropy:(!isNaN(entropy) ? entropy : 1)};
-	}
-	function calculateFrequencies(table_training){
-		var table_frequencies={},row_frequencies={};		
-		for(var x=0,attributes_label_length=attributes_label.length;x<attributes_label_length;x++){
-			table_frequencies[attributes_label[x]]={};
-			if(attributes_label[x]==class_defined)
-					continue;
-			for(var y=0,attributes_length=attributes[attributes_label[x]].length;y<attributes_length;y++){
-				row_frequencies[attributes_label[x]]={};
-				row_frequencies[attributes_label[x]][attributes[attributes_label[x]][y]]=[];				
-				for(var i=0,table_training_length=table_training.length;i<table_training_length;i++)
-					if(table_training[i][attributes_label[x]]==attributes[attributes_label[x]][y])
-						row_frequencies[attributes_label[x]][attributes[attributes_label[x]][y]].push(table_training[i]);
-				table_frequencies[attributes_label[x]][attributes[attributes_label[x]][y]]=calculateFrequencieClass(row_frequencies[attributes_label[x]][attributes[attributes_label[x]][y]]);				
-				table_frequencies[attributes_label[x]][attributes[attributes_label[x]][y]]["table"]=row_frequencies[attributes_label[x]][attributes[attributes_label[x]][y]];
-				row_frequencies={};
+		tablas_filtradas.push(nueva_tabla);
+	});
+	atributos_nombres.splice(atributos_nombres.indexOf(atributo),1);
+	tablas_filtradas.forEach(function(tabla_filtrada){		
+		console.dir(tabla_filtrada);
+		if(!parent.esHomogenea(tabla_filtrada)){
+			if(tabla_filtrada.length>2)
+				parent.getTree(tabla_filtrada,atributos_nombres);
+			else{
+				console.log("Termina aqui");
+				console.dir(tabla_filtrada);
 			}
+		}else{
+			console.log("Fue homogenea");
 		}
-		return table_frequencies;
-	}
-	function reduceSet(parent_node,attribute_name,repeat,table_training){
-		new_table=[];
-		if(repeat<7 && attributes[attribute_name]!=undefined){
-			for(var i=0,length=attributes[attribute_name].length;i<length;i++){
-				for(x=0,length_table=table_training.length;x<length_table;x++){
-					if(table_training[x][attribute_name]==attributes[attribute_name][i]){
-						 //EN LUGAR DE ELIMINAR AQUI EL ATRIBUTO CON MAYOR GANANCIA
-						 // MEJOR ELIMINAR EL ATRIBUTO DEL attributes_label
-						attributes_label=attributes_label.filter(function(element){
-							return element!=attribute_name;
-						});
-						new_table.push(table_training[x]);
-					}
-				}				
-				repeat++;
-				if(!isHomogeneous(new_table,attributes[attribute_name][i])){
-					Tree.add(attributes[attribute_name][i],attribute_name);		
-					execute(attributes[attribute_name][i],repeat,new_table);
-				}else{
-					if(parent_node==""){
-						parent_node=attribute_name;
-					}
-					Tree.addLeaf(attributes[attribute_name][i],parent_node,new_table[0][class_defined],attribute_name);
-					attributes[attribute_name].splice(i,1);	
-					i=i-1;	
-					length=attributes[attribute_name].length;
-				}
-				new_table=[];
-			}
+	})
+}
+
+id3.prototype.conteoDominio=function(atributos_nombres,entropia_conjunto,tabla_entrenamiento){
+	var parent=this;	
+	ganancias={};
+	console.log
+	atributos_nombres.forEach(function(atributo){		
+		if(atributo!=parent.clase){
+			ganancias[atributo]=0
+			frecuencias=parent.getFrecuencias(atributo,parent.atributos,tabla_entrenamiento);
+			ganancias[atributo]=entropia_conjunto-parent.getEntropiaAtributo(atributo,parent.conteoTotales(atributo,frecuencias[atributo]));
+		}
+	});
+	atributo_mayor="";
+	atributo_valor=0;
+	for(var atributo in ganancias){
+		if(ganancias[atributo]>atributo_valor){
+			atributo_mayor=atributo;
+			atributo_valor=ganancias[atributo];
 		}
 	}
-	/* Filter all the elements with the same class name, if there are more that one where the
-		the class name are differente,is because this is no homogeneous
-	*/
-	function isHomogeneous(table_frequencies,higher_attribute){
-		var key="",attribute_homogeneous_value="";
-		filter=table_frequencies.filter(function(element){
-			attribute_homogeneous_value=element[higher_attribute];
-			if(key.length>0){
-				return key!=element[class_defined];
-			}else{
-				key=element[class_defined];
-				return false;
-			}
-		});
-		return filter.length==0;
-	}
-	/* If the set are only two elements, there is not reason to calculate the entropy */
-	function splitSet(node_value,table_training){
-		attribute_dispatch="",attribute_origin="";
-		for(var i=0;i<table_training.length;i++){
-			for(var attribute in table_training[i]){
-				if(table_training[i][attribute]==node_value){
-					attribute_origin=attribute.toString();
-					break;						
-				}	
-				attribute_dispatch=attribute.toString();
-			}
-			Tree.addLeaf(node_value,attribute_dispatch,table_training[i][class_defined],attribute_origin);			
-		}
-	}
-	function execute(parent_node,repeat,table_training){
-		table_frequencies=calculateFrequencies(table_training);		
-		frequencie_class_for_set=calculateFrequencieClass(table_training);		
-		entropy_of_set=calculateEntropySet(frequencie_class_for_set);			
-		/* Calculate the entropies of each of the attributes in the set */	
-		higher_entropy=0;	
-		higher_entropy_name="";
-		entropies=[];
-		if(table_training.length>2){
-			for(var i=0,attribute_label_name=attributes_label[i],attributes_label_length=attributes_label.length;i<attributes_label_length;i++,attribute_label_name=attributes_label[i]){
-				if(attributes_label[i]==class_defined)
-					continue;			
-				table_frequencies[attribute_label_name]["entropy"]=0
-				for(var x=0,attributes_length=attributes[attribute_label_name].length;x<attributes_length;x++){
-					table_frequencie=table_frequencies[attribute_label_name][attributes[attribute_label_name][x]];
-					table_frequencies[attribute_label_name]["entropy"]+=(table_frequencie["total"]/frequencie_class_for_set["total"])*calculateEntropySet(table_frequencie)["entropy"];
-				}
-				table_frequencies[attribute_label_name]["entropy"]=entropy_of_set["entropy"]-table_frequencies[attribute_label_name]["entropy"];			
-				if(higher_entropy<table_frequencies[attribute_label_name]["entropy"]){
-					higher_entropy=table_frequencies[attribute_label_name]["entropy"];
-					higher_entropy_name=attribute_label_name;
-				}			
-			}
-			reduceSet(parent_node,higher_entropy_name,repeat,table_training);
-		}else
-			splitSet(parent_node,table_training);		
-		
-	}
-	return{
-		init:function(data){
-			Math.log2 = Math.log2 || function(x){return Math.log(x)*Math.LOG2E;};
-			class_defined=data;
-			execute("",1,table_training);
-		},		
-		getFrequencieClass:function(){
-			console.dir(calculateFrequencieClass(table_training));
-		},
-		getFrequencies:function(){
-			console.dir(calculateFrequencies(table_training));
-		},
-		getEntropy:function(){
-			console.dir(calculateEntropySet({ total: 14, no: 4, si: 10 }));
-		},
-		getTree:function(){
-			console.dir(Tree.getStack());
-			console.dir(Tree.getBranches());
-		}
-	}
-})();
-module.exports=ID3;
+	console.log("El atributo mayor "+atributo_mayor+" "+atributo_valor);
+	this.filtradoAtributo(atributo_mayor,tabla_entrenamiento,atributos_nombres);
+}
+
+id3.prototype.initBuild=function(){
+	this.getTree(this.tabla_entrenamiento,this.atributos_nombres);
+}
+
+id3.prototype.getTree=function(tabla_entrenamiento,atributos_nombres){
+	entropia_conjunto=this.getEntropia(this.conteoEntrenamiento(tabla_entrenamiento));
+	this.conteoDominio(atributos_nombres,entropia_conjunto,tabla_entrenamiento);	
+}
+
+id3.prototype.esHomogenea=function(probar_homogenea){
+	var conteo={};
+	var parent=this;
+	conteo[this.atributos[this.clase][0]]={};
+	conteo[this.atributos[this.clase][0]]["diferentes"]=0;
+	conteo[this.atributos[this.clase][0]]["iguales"]=0;
+	probar_homogenea.forEach(function(entrada){
+		if(entrada[parent.clase]==parent.atributos[parent.clase][0])
+			conteo[parent.atributos[parent.clase][0]]["iguales"]++;
+		else
+			conteo[parent.atributos[parent.clase][0]]["diferentes"]++;
+	});
+	return (conteo[this.atributos[this.clase][0]]["iguales"]==0 || conteo[this.atributos[this.clase][0]]["diferentes"]==0);	
+}
+
+
+module.exports=id3;
